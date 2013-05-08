112bd341c97f58f30202ca982b210478219be3f1
Michael Henretty <michael.henretty@gmail.com>
2013-05-08 16:04:37 -0700

    use a clone of this repo for configuration

--- tools/boot.sh
+++ tools/boot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+SRC_DIR=~/.mikehenrty-boot
+
 fail() {
 	echo $1
 	exit 1
 }
-
-SRC_DIR=`pwd`
 
 #git
 [[ `which git` ]] || fail "Please install git first."
@@ -15,6 +15,10 @@ git config --global user.name "Michael Henretty"
 git config --global user.email "michael.henretty@gmail.com"
 git config --global color.ui auto
 git config --global core.editor vim
+
+rm -rf $SRC_DIR
+mkdir -p $SRC_DIR
+git clone https://github.com/mikehenrty/boot $SRC_DIR
 
 #vim config
 ln -s $SRC_DIR/.vimrc ~/.vimrc
